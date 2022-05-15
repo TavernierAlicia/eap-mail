6d0686f1b8278b0c605cf24a44d7e35025b69539
«Alicia» <alicia@ringover.com>
2022-05-15 15:42:30 +0200

    WIP

--- eapMAIL.go
+++ eapMAIL.go
@@ -252,6 +252,31 @@ func SuspendCreanceMail(etab eapFact.FactEtab, facts Unpaid) (err error) {
 	return err
 }
 
+func AskDeleteAccount(etab eapFact.FactEtab, echeance string) (err error) {
+	to := etab.Mail
+	from := viper.GetString("sendmail.service_mail")
+	pass := viper.GetString("sendmail.service_pwd")
+
+	subject := "Supression du compte - EAP"
+
+	message := "Bonjour, " + etab.Owner_civility + " " + etab.Owner_name + ", \n Nous avons bien enregistré votre demande." +
+		"Votre compte et votre abonnement seront supprimés au dernier jour de votre abonnement, c'est à dire le " + echeance +
+		", après cette date, un récapitulatif de vos factures et autres données vous sera envoyé avec le mail de confirmation. Vous pouvez toujours annuler cette action en mettant à jour votre abonnement sur votre espace dédié."
+	m := gomail.NewMessage()
+	m.SetHeader("From", from)
+	m.SetHeader("To", to)
+	m.SetHeader("Subject", subject)
+	m.SetBody("text/html", message)
+
+	d := gomail.NewPlainDialer("smtp.gmail.com", 587, from, pass)
+
+	if err := d.DialAndSend(m); err != nil {
+		fmt.Println(err)
+	}
+
+	return err
+}
+
 func ConfirmDeleteAccount(etab eapFact.FactEtab) (err error) {
 	to := etab.Mail
 	from := viper.GetString("sendmail.service_mail")
@@ -260,7 +285,7 @@ func ConfirmDeleteAccount(etab eapFact.FactEtab) (err error) {
 	subject := "Compte supprimé - EAP"
 
 	message := "Bonjour, " + etab.Owner_civility + " " + etab.Owner_name + ", \n Suite à votre demande, votre compte a bien été supprimé, ainsi que toutes les informations qu'il contenait." +
-		"Nous vous souhaitons une bonne continuation et, peut-être à bientôt?"
+		" Nous vous souhaitons une bonne continuation et, peut-être à bientôt?"
 	m := gomail.NewMessage()
 	m.SetHeader("From", from)
 	m.SetHeader("To", to)
